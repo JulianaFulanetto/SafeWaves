dfc4394d93aa4641dfab7fe0e7ee222411a1ad5b
Juliana <juliana.fulanetto@aluno.senai.br>
2025-09-17 17:04:51 -0300

    Adicionando card

--- App.js
+++ App.js
@@ -2,14 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { useTheme } from "./hooks/useTheme";
 
-
 export default function App() {
   const styles = createStyles(useTheme());
 
   return (
     <View style={styles.container}>
-      <Text>Safe Waves</Text>
       <StatusBar style="auto" />
+      <View style={styles.card}></View>
     </View>
   );
 }
