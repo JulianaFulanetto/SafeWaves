ba6bb2853d8c5c26da755b0d6b7229209a4f5bae
Juliana <juliana.fulanetto@aluno.senai.br>
2025-09-17 11:44:10 -0300

    Alterando a coloração

---
+++ themes/dark.js
@@ -0,0 +1,10 @@
+export const darkTheme = {
+    background: "#B3CFE5",
+    card: "#05162F",
+    text: "#B3CFE5",
+    border: "#264867",
+    buttonPrincipal: "#102E50",
+    buttonSecundario: "#071B3D",
+    shadow:"#00000",
+  };
+  
---
+++ themes/light.js
@@ -0,0 +1,11 @@
+export const lightTheme = {
+    background: "#A8D0EF",
+    card: "#80A4C7",
+    text: "#021024",
+    border: "#647E9F",
+    buttonPrincipal: "#6DA5D1",
+    buttonSecundario: "#B3CFE5",
+    shadow:"#4A7FA7",
+  };
+  
+  
