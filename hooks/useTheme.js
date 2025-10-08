14a816294f6d7c3f39cedc1418071f6d5deec263
Juliana <juliana.fulanetto@aluno.senai.br>
2025-10-08 10:21:56 -0300

    Remove useTheme hook and light theme definition to simplify theme management

--- hooks/useTheme.js
+++
@@ -1,8 +0,0 @@
-import { useColorScheme } from "react-native";
-import { darkTheme } from "../themes/dark";
-import { lightTheme } from "../themes/light";
-
-export const useTheme = () => {
-  const scheme = useColorScheme();
-  return scheme === "dark" ? darkTheme : lightTheme;
-};
